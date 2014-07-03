bec67d7d29f321a70ead7e35cac60c3ff2c4cbf4
Rafael <Rafael@Rafael-PC>
2014-07-02 21:02:26 -0300

    Utils final

--- src/java/util/Utils.java
+++ src/java/util/Utils.java
@@ -1,10 +1,15 @@
 package util;
 
+import controler.InscricaoDAO;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
+import model.Inscricao;
 
 public class Utils {
 
@@ -31,5 +36,25 @@ public class Utils {
 
     public static HttpSession retornaSessao() {
         return (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+    }
+
+    public static void atualizarInscricoes() {
+        for (Inscricao inscricao : new InscricaoDAO().listarTodos()) {
+            try {
+                ParseFeed.converterFeed(inscricao);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                atualizarInscricoes();
+            }
+        }, 0, 60000 * 30);
     }
 }
